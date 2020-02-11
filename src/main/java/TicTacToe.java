c2af9e3cc7e7f362efbcae6e8d3eb681d3b3ada4
Sneha Narendran <sneha.narendran1@thoughtworks.com>
2020-02-11 14:44:19 +0530

    Add|Player one wins if he has all artifacts in the same row|

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,14 @@
+public class TicTacToe {
+
+    private final Player playerOne;
+    private final Player playerTwo;
+
+    public TicTacToe(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+
+    public Player play() {
+        return playerOne;
+    }
+}
